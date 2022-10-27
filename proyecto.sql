e054cd75cf2c179ce491394c61dfd81ff1c07f01
ARIEL CARABALLO DIAZ <acaraballod21@curnvirtual.edu.co>
2022-10-27 17:09:31 -0500

    cambio en base de datos

--- proyecto.sql
+++ proyecto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-10-2022 a las 22:43:05
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.12
+-- Tiempo de generación: 28-10-2022 a las 00:08:56
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `proyectodegrado`
+-- Base de datos: `proyecto`
 --
 
 -- --------------------------------------------------------
@@ -31,9 +31,15 @@ CREATE TABLE `estudiantes` (
   `id` int(10) NOT NULL,
   `Nombre` varchar(255) NOT NULL,
   `documento` int(15) NOT NULL,
-  `id_modulo` int(10) NOT NULL,
   `id_lista` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `estudiantes`
+--
+
+INSERT INTO `estudiantes` (`id`, `Nombre`, `documento`, `id_lista`) VALUES
+(2, 'Ariel Caraballo Diaz', 1001903378, 29);
 
 -- --------------------------------------------------------
 
@@ -44,8 +50,19 @@ CREATE TABLE `estudiantes` (
 CREATE TABLE `listas` (
   `id` int(10) NOT NULL,
   `nombre` varchar(255) NOT NULL,
-  `fecha` varchar(60) NOT NULL
+  `fecha` varchar(60) NOT NULL,
+  `semestre` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `listas`
+--
+
+INSERT INTO `listas` (`id`, `nombre`, `fecha`, `semestre`) VALUES
+(27, 'Lista grupo 1', '27/10/22', '2'),
+(28, 'Grupo 2', '27/10/22', '5'),
+(29, 'lista 3', '27/10/22', '2'),
+(30, 'Grupo 20', '28/10/22', '14');
 
 -- --------------------------------------------------------
 
@@ -57,6 +74,21 @@ CREATE TABLE `modulos` (
   `id` int(10) NOT NULL,
   `nombre` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `modulos`
+--
+
+INSERT INTO `modulos` (`id`, `nombre`) VALUES
+(1, 'Promoción y prevención'),
+(2, 'Actividades basicas'),
+(3, 'Cuidado medico'),
+(4, 'Cuidado materno infantil'),
+(5, 'Practicas administracion'),
+(6, 'Adulto mayor'),
+(7, 'Practica integral'),
+(8, 'Fundamentos socioeducativo IV'),
+(9, 'Fundamentos socioeducativo V');
 
 -- --------------------------------------------------------
 
@@ -80,7 +112,8 @@ CREATE TABLE `modulo_nota` (
 CREATE TABLE `permisos` (
   `id` int(10) NOT NULL,
   `id_modulo` int(10) NOT NULL,
-  `id_docente` int(10) NOT NULL
+  `id_docente` int(10) NOT NULL,
+  `id_lista` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -101,6 +134,15 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `correo`, `clave`, `ultima_sesion`, `documento`, `fecha`) VALUES
+(1, 'Ariel', 'Caraballo', 'ariel@gmail.com', '123456', '25/10/2022', 1001903378, '25/10/2022'),
+(2, 'JUAN', 'Caraballo', 'juan@gmail.com', '123456', '25/10/2022', 12345678, '25/10/2022'),
+(3, 'roberto', 'caraballo', 'roberto@gmail.com', '123456', '25/10/2022', 1001903378, '25/10/2022');
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -109,8 +151,7 @@ CREATE TABLE `usuarios` (
 --
 ALTER TABLE `estudiantes`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_lista` (`id_lista`),
-  ADD KEY `fk_modulo` (`id_modulo`);
+  ADD KEY `fk_lista` (`id_lista`);
 
 --
 -- Indices de la tabla `listas`
@@ -129,8 +170,7 @@ ALTER TABLE `modulos`
 --
 ALTER TABLE `modulo_nota`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_estudiante` (`id_estudiante`),
-  ADD KEY `fk_modulo1` (`id_modulo`);
+  ADD KEY `fk_estudiante` (`id_estudiante`);
 
 --
 -- Indices de la tabla `permisos`
@@ -138,7 +178,8 @@ ALTER TABLE `modulo_nota`
 ALTER TABLE `permisos`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_docentes` (`id_docente`),
-  ADD KEY `fk_modulos` (`id_modulo`);
+  ADD KEY `fk_modulos` (`id_modulo`),
+  ADD KEY `fk_lista1` (`id_lista`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -154,19 +195,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `estudiantes`
 --
 ALTER TABLE `estudiantes`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `listas`
 --
 ALTER TABLE `listas`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT de la tabla `modulos`
 --
 ALTER TABLE `modulos`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `modulo_nota`
@@ -184,7 +225,7 @@ ALTER TABLE `permisos`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
@@ -194,21 +235,20 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `estudiantes`
 --
 ALTER TABLE `estudiantes`
-  ADD CONSTRAINT `fk_lista` FOREIGN KEY (`id_lista`) REFERENCES `listas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_modulo` FOREIGN KEY (`id_modulo`) REFERENCES `modulos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_lista` FOREIGN KEY (`id_lista`) REFERENCES `listas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `modulo_nota`
 --
 ALTER TABLE `modulo_nota`
-  ADD CONSTRAINT `fk_estudiante` FOREIGN KEY (`id_estudiante`) REFERENCES `estudiantes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_modulo1` FOREIGN KEY (`id_modulo`) REFERENCES `modulos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_estudiante` FOREIGN KEY (`id_estudiante`) REFERENCES `estudiantes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `permisos`
 --
 ALTER TABLE `permisos`
   ADD CONSTRAINT `fk_docentes` FOREIGN KEY (`id_docente`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_lista1` FOREIGN KEY (`id_lista`) REFERENCES `listas` (`id`),
   ADD CONSTRAINT `fk_modulos` FOREIGN KEY (`id_modulo`) REFERENCES `modulos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
