5d1d5fb9152e14cfdf12f5313d76d4d16a89c5b6
Ariel Caraballo Diaz <ariel.cdiaz321@gmail.com>
2023-03-07 12:44:36 -0500

    cambios

--- pages/config/listas-modulo.php
+++ pages/config/listas-modulo.php
@@ -1,7 +1,8 @@
 <?php
-$sqllistas = "SELECT * from listas WHERE id_modulo = ? ORDER BY id DESC ";
+$sqllistas = "SELECT * from listas WHERE id_modulo = ? AND id_usuario = ? ORDER BY id DESC ";
 $sentencialistas = $mbd->prepare($sqllistas);
 $sentencialistas->bindParam(1, $idmodulo);
+$sentencialistas->bindParam(2, $idglobal);
 $sentencialistas->execute();
 $filas2 = $sentencialistas->rowCount();
 $listas = $sentencialistas->fetchAll();
